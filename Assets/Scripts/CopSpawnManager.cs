7f8c77bbf98edc583a6d5bc1d5c3137772da29a1
Philip Nazo <philip.nazo@hotmail.com>
2017-05-23 13:46:28 +1200

    CopSpawn
    Spawning works for one cop

---
+++ Assets/Scripts/CopSpawnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CopSpawnManager : MonoBehaviour
+{
+
+    public Object Cop_Standard, Cop_Fast, Cop_Tank;
+    public Vector3 spawnValues;
+    public float Lane1, Lane2, Lane3, Lane4, Lane5;
+    public int copCount;
+    public float spawnWait, startWait, waveWait;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(spawnWaves());
+    }
+
+    IEnumerator spawnWaves()
+    {
+        Debug.Log("I was hit");
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < copCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(8, Random.Range(Lane1, Lane5), 0); //2.35f Top lane
+                Instantiate(Cop_Standard, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+
+    }
+
+    //    IEnumerator spawnWaves()
+    //    {
+    //        yield return new WaitForSeconds(startWait);
+    //        while(true)
+    //        {
+    //            for(int i = 0; i < copCount; i++)
+    //            {
+    //                switch()
+    //            }
+    //        }
+    //    }
+    //}
+
+}
